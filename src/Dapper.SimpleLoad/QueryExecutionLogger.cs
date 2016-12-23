5a39b12afc76a071c22d08f643771afe5a399997
Bart Read <bart@bartread.com>
2016-12-23 15:49:20 +0000

    Improved logging of long running queries, and queries that return a large number of results.

---
+++ src/Dapper.SimpleLoad/QueryExecutionLogger.cs
@@ -0,0 +1,128 @@
+﻿using log4net;
+using Newtonsoft.Json;
+using System;
+
+namespace Dapper.SimpleLoad
+{
+    public class QueryExecutionLogger
+    {
+        private static readonly ILog Log = LogManager.GetLogger(typeof(QueryExecutionLogger));
+
+        private enum ThresholdBreach
+        {
+            None,
+            Warn,
+            Error,
+            Throw
+        }
+
+        public static void Executing(IQuery query, object parameters)
+        {
+            if (Log.IsInfoEnabled)
+            {
+                Log.Info("Executing query:\r\n" + query.Sql);
+                Log.Info("Split on: " + query.SplitOn);
+
+                if (SimpleLoadConfiguration.IncludeParametersInLog)
+                {
+                    Log.Info("Parameters: " + JsonConvert.SerializeObject(parameters));
+                }
+            }
+        }
+
+        public static void Executed(IQuery query, object parameters, int rowCount, long startTime, long timeToFirstResult)
+        {
+            var totalTime = (DateTime.Now.Ticks - startTime) / TimeSpan.TicksPerMillisecond;
+            var breach = DecideOnBreachLevel(rowCount, totalTime);
+
+            if (breach != ThresholdBreach.None)
+            {
+                var message = $@"RECEIVED {rowCount} ROWS IN QUERY RESULT SET IN {totalTime}ms FROM QUERY:
+{query.Sql}
+CALLING STACK TRACE:
+{Environment.StackTrace}";
+
+                if (SimpleLoadConfiguration.IncludeParametersInLog)
+                {
+                    message += Environment.NewLine
+                        + "Parameters: "
+                        + Environment.NewLine
+                        + JsonConvert.SerializeObject(parameters);
+                }
+
+                switch (breach)
+                {
+                    case ThresholdBreach.Throw:
+                        if (Log.IsFatalEnabled)
+                        {
+                            Log.Fatal(message);
+                        }
+                        throw new AnnotatedSqlException(message, query.Sql, query.SplitOn, parameters);  //  TODO: what exception to throw
+
+                    case ThresholdBreach.Error:
+                        if (Log.IsErrorEnabled)
+                        {
+                            Log.Error(message);
+                        }
+                        break;
+
+                    case ThresholdBreach.Warn:
+                    default:
+                        if (Log.IsWarnEnabled)
+                        {
+                            Log.Warn(message);
+                        }
+                        break;
+                }
+            }
+            else
+            {
+                if (Log.IsInfoEnabled)
+                {
+                    Log.Info(string.Format(
+                        "Received {0} rows in query result set in {1}ms.",
+                        rowCount,
+                        totalTime));
+                }
+            }
+        }
+
+        private static ThresholdBreach DecideOnBreachLevel(int rowCount, long totalTime)
+        {
+            var breach = ThresholdBreach.None;
+
+            if (BreachesThreshold(
+                rowCount,
+                SimpleLoadConfiguration.RowCountExceptionThrowThreshold,
+                totalTime,
+                SimpleLoadConfiguration.QueryDurationMillisExceptionThrowThreshold))
+            {
+                breach = ThresholdBreach.Throw;
+            }
+            else if (BreachesThreshold(
+                rowCount,
+                SimpleLoadConfiguration.RowCountErrorEmitThreshold,
+                totalTime,
+                SimpleLoadConfiguration.QueryDurationMillisExceptionThrowThreshold))
+            {
+                breach = ThresholdBreach.Error;
+            }
+            else if (BreachesThreshold(
+                rowCount,
+                SimpleLoadConfiguration.RowCountErrorEmitThreshold,
+                totalTime,
+                SimpleLoadConfiguration.QueryDurationMillisExceptionThrowThreshold))
+            {
+                breach = ThresholdBreach.Warn;
+            }
+
+            return breach;
+        }
+
+        private static bool BreachesThreshold(int rowCount, int rowCountThreshold, long totalTime, long totalTimeThreshold)
+        {
+            return ((rowCountThreshold > 0 && rowCount >= rowCountThreshold)
+                    || (totalTimeThreshold > 0 && totalTime >= totalTimeThreshold));
+        }
+    }
+}
---
+++ src/Dapper.SimpleLoad/SimpleLoadConfiguration.cs
@@ -0,0 +1,126 @@
+﻿namespace Dapper.SimpleLoad
+{
+    /// <summary>
+    /// Defines configuration settings which alter SimpleLoad's behaviour. Currently all
+    /// relates to logging and how SimpleLoad should handle long running queries, and queries
+    /// that handle large results sets.
+    /// </summary>
+    public static class SimpleLoadConfiguration
+    {
+        private static int _rowCountWarningEmitThreshold;
+        private static int _rowCountErrorEmitThreshold;
+        private static int _rowCountExceptionThrowThreshold;
+
+        private static long _queryDurationMillisWarningEmitThreshold;
+        private static long _queryDurationMillisErrorEmitThreshold;
+        private static long _queryDurationMillisExceptionThrowThreshold;
+
+        static SimpleLoadConfiguration()
+        {
+            RowCountWarningEmitThreshold = 1000;
+            RowCountErrorEmitThreshold = 2000;
+            RowCountExceptionThrowThreshold = 0;    //  I.e., don't ever thrown an exception
+            QueryDurationMillisWarningEmitThreshold = 2000;
+            QueryDurationMillisErrorEmitThreshold = 4000;
+            QueryDurationMillisExceptionThrowThreshold = 0;     //  I.e., don't ever thrown an exception
+        }
+
+        /// <summary>
+        /// Number of rows in result set above which SimpleLoad will log a warning.
+        /// Ignored if set to 0.
+        /// </summary>
+        public static int RowCountWarningEmitThreshold
+        {
+            get { return _rowCountWarningEmitThreshold; }
+            set
+            {
+                value.IsGreaterThanOrEqualTo(nameof(RowCountWarningEmitThreshold), 0);
+                _rowCountWarningEmitThreshold = value;
+            }
+        }
+
+        /// <summary>
+        /// Number of rows in result set above which SimpleLoad will log an error.
+        /// Ignored if set to 0.
+        /// </summary>
+        public static int RowCountErrorEmitThreshold
+        {
+            get { return _rowCountErrorEmitThreshold; }
+            set
+            {
+                value.IsGreaterThanOrEqualTo(nameof(RowCountErrorEmitThreshold), 0);
+                _rowCountErrorEmitThreshold = value;
+            }
+        }
+
+        /// <summary>
+        /// Number of rows in result set above which SimpleLoad will throw an exception.
+        /// Ignored if set to 0.
+        /// </summary>
+        public static int RowCountExceptionThrowThreshold
+        {
+            get { return _rowCountExceptionThrowThreshold; }
+            set
+            {
+                value.IsGreaterThanOrEqualTo(nameof(RowCountExceptionThrowThreshold), 0);
+                _rowCountExceptionThrowThreshold = value;
+            }
+        }
+
+        /// <summary>
+        /// Total duration of query and mapping above which SimpleLoad will log a warning.
+        /// Ignored if set to 0.
+        /// </summary>
+        public static long QueryDurationMillisWarningEmitThreshold
+        {
+            get { return _queryDurationMillisWarningEmitThreshold; }
+            set
+            {
+                value.IsGreaterThanOrEqualTo(nameof(QueryDurationMillisWarningEmitThreshold), 0);
+                _queryDurationMillisWarningEmitThreshold = value;
+            }
+        }
+
+        /// <summary>
+        /// Total duration of query and mapping above which SimpleLoad will log an error.
+        /// Ignored if set to 0.
+        /// </summary>
+        public static long QueryDurationMillisErrorEmitThreshold
+        {
+            get { return _queryDurationMillisErrorEmitThreshold; }
+            set
+            {
+                value.IsGreaterThanOrEqualTo(nameof(QueryDurationMillisErrorEmitThreshold), 0);
+                _queryDurationMillisErrorEmitThreshold = value;
+            }
+        }
+
+        /// <summary>
+        /// Total duration of query and mapping above which SimpleLoad will throw an exception.
+        /// Ignored if set to 0.
+        /// </summary>
+        public static long QueryDurationMillisExceptionThrowThreshold
+        {
+            get { return _queryDurationMillisExceptionThrowThreshold; }
+            set
+            {
+                value.IsGreaterThanOrEqualTo(nameof(QueryDurationMillisExceptionThrowThreshold), 0);
+                _queryDurationMillisExceptionThrowThreshold = value;
+            }
+        }
+
+        /// <summary>
+        /// Indicates whether or not query parameters should be included in log output. Off by
+        /// default and should not be switched on unless you are certain your query parameters
+        /// do not contain sensitive information.
+        /// </summary>
+        public static bool IncludeParametersInLog { get; set; }
+
+        /// <summary>
+        /// Indicates whether or not query parameters should be included in exceptions. Off by
+        /// default and should not be switched on unless you are certain your query parameters
+        /// do not contain sensitive information.
+        /// </summary>
+        public static bool IncludeParametersInException { get; set; }
+    }
+}
